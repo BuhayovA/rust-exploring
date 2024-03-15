7dc4755329a5c25304b8d4924f51d17e3a673eb0
Alexey Buhayov <neznaykagm@gmail.com>
2024-03-15 08:49:12 +0200

    added write_file func

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::io::*;
+#![allow(unused)]
+
 use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+use std::io::prelude::*;
+
+fn write_file() -> std::io::Result<()> {
+    let mut file = File::create("foo.txt")?;
+    file.write_all(b"Hello, world!");
+    Ok(())
+}
 
 fn main() {
+    write_file();
+
     println!("Hello, world!");
 }
